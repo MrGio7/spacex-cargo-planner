872c3f5d5126465581f0895b515ac4c760185b67
MrGio7 <lashagiorgi1233@gmail.com>
2021-12-08 15:02:17 +0400

    added search bar visual

--- src/components/blocks/search.js
+++ src/components/blocks/search.js
@@ -1,9 +1,10 @@
 import React from "react";
+import zoom from "../../assets/zoom.svg";
 
 const Search = () => {
   return (
     <div className="search">
-      <input placeholder="Search" />
+      <input placeholder="Search" style={{backgroundImage: `url(${zoom})`}} />
     </div>
   );
 };
