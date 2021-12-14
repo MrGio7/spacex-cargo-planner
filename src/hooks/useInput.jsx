4ec4d462a977e9e1e7aac8eecd59d4f8d484fe63
MrGio7 <lashagiorgi1233@gmail.com>
2021-12-14 21:02:21 +0400

    trying to fix bug

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import {useState} from "react";
 
 export default function useInput(initialValue) {
   const [value, setValue] = useState(initialValue);
